58c2d0f9cebc566624cbb75105cbb57974a76c18
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-08-21 15:10:58 -0300

    Adjustments

--- app.js
+++ app.js
@@ -8,12 +8,61 @@ let timeOut = duration * 1100;
 function openModal() {
   modal.style.display = "block";
   backdrop.style.display = "block";
-  gsap.fromTo(modal, duration, { opacity: 1, y: 40 }, { opacity: 1, y: 0 });
-  gsap.fromTo(backdrop, duration, { opacity: 1, y: 40 }, { opacity: 1, y: 0 });
+  gsap.fromTo(
+    modal,
+    duration,
+    {
+      opacity: 1,
+      y: 40,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      ease: Power1.easeOut
+    }
+  );
+  gsap.fromTo(
+    backdrop,
+    duration,
+    {
+      opacity: 1,
+      y: 40,
+    },
+    {
+      opacity: 1,
+      y: 0,
+      ease: Power1.easeOut
+    }
+  );
 }
+
 function closeModal() {
-  gsap.fromTo(modal, duration, { opacity: 1, y: 0 }, { opacity: 0, y: 40 });
-  gsap.fromTo(backdrop, duration, { opacity: 1, y: 0 }, { opacity: 0, y: 40 });
+  gsap.fromTo(
+    modal,
+    duration,
+    {
+      opacity: 1,
+      y: 0,
+    },
+    {
+      opacity: 0,
+      y: 40,
+      ease: Power1.easeOut
+    }
+  );
+  gsap.fromTo(
+    backdrop,
+    duration,
+    {
+      opacity: 1,
+      y: 0,
+    },
+    {
+      opacity: 0,
+      y: 40,
+      ease: Power1.easeOut
+    }
+  );
 
   setTimeout(function () {
     modal.style.display = "none";
