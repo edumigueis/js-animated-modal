4a71de9e7ae5af25b6baced28ce1b7648fb5960c
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-08-21 14:57:26 -0300

    Small animation

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+var modal = document.querySelector(".modal");
+var backdrop = document.querySelector(".backdrop");
+modal.style.opacity = 0;
+backdrop.style.opacity = 0;
+let duration = 0.5;
 
-var modal = document.querySelector('.modal');
-var backdrop = document.querySelector('.backdrop');
-
-function openModal(){
+function openModal() {
   modal.style.display = "block";
   backdrop.style.display = "block";
+  gsap.to(modal, duration, { opacity: 1 });
+  gsap.to(backdrop, duration, { opacity: 1 });
 }
-function closeModal(){
-  
+function closeModal() {
+  gsap.to(modal, duration, { opacity: 0 });
+  gsap.to(backdrop, duration, { opacity: 0 });
+
+  let timeOut = duration * 130;
+  setTimeout(function () {
+    modal.style.display = "none";
+    backdrop.style.display = "none";
+  }, timeOut);
 }
