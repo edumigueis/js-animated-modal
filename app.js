7d055dbd6438de50d2c048191acc8e7fd5da14f1
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-08-21 15:41:57 -0300

    Second Modal

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let duration = 0.5;
 let timeOut = duration * 1100;
 let quote = document.getElementById("quote");
 let message = document.getElementById("message");
+const imgModal = document.querySelector(".img-modal");
+imgModal.style.opacity = 0;
 
 function openModal() {
   modal.style.display = "block";
@@ -39,10 +41,13 @@ function openModal() {
 }
 
 function closeModal(event) {
+  if(imgModal.classList.contains("open")){
+    closeImgModal();
+    return;
+  }
   if (quote.value != "" && !event.target.classList.contains("btn-cancel")) {
     message.innerHTML = quote.value;
   }
-
   gsap.fromTo(
     modal,
     duration,
@@ -74,4 +79,69 @@ function closeModal(event) {
     modal.style.display = "none";
     backdrop.style.display = "none";
   }, timeOut);
+}
+
+function openImgModal(event){
+  imgModal.style.display = "block";
+  imgModal.classList.add("open");
+  document.getElementById("modal-img").setAttribute("src", event.target.src)
+  gsap.fromTo(
+    imgModal,
+    duration,
+    {
+      opacity: 0,
+      y: 0,
+    },
+    {
+      opacity: 1,
+      y: 40,
+      ease: Power1.easeOut,
+    }
+  );
+  gsap.fromTo(
+    backdrop,
+    0,
+    {
+      zIndex: 10
+    },
+    {
+      zIndex: 25,
+      ease: Power1.easeOut,
+    }
+  );
+}
+
+function closeImgModal(){
+  imgModal.classList.add("open");
+  document.getElementById("modal-img").setAttribute("src", event.target.src)
+  gsap.fromTo(
+    imgModal,
+    duration,
+    {
+      opacity: 1,
+      y: 40,
+    },
+    {
+      opacity: 0,
+      y: 0,
+      ease: Power1.easeOut,
+    }
+  );
+  gsap.fromTo(
+    backdrop,
+    0,
+    {
+      zIndex: 25
+    },
+    {
+      zIndex: 10,
+      ease: Power1.easeOut,
+    }
+  );
+
+  setTimeout(function(){
+    imgModal.style.display = "block";
+  }, timeOut)
+
+  imgModal.classList.remove("open");
 }
