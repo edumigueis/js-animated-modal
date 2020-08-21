8e4fc701d53f6cac2275136c501bb7e70c5b2ae5
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-08-21 14:46:04 -0300

    Basic Structure

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var modal = document.querySelector('.modal');
+var backdrop = document.querySelector('.backdrop');
+
+function openModal(){
+  modal.style.display = "block";
+  backdrop.style.display = "block";
+}
+function closeModal(){
+  alert("u");
+}
